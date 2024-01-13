070ca2b20d2123d86c0fa743252b41f7735357cb
ElementaryLewis <lewis.p.mu@outlook.com>
2024-01-13 12:57:36 +0100

    Ajout d'un BDD en query/SQL

--- index.php
+++ index.php
@@ -1,215 +1,125 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;dbname=blog', 'root', '+Koppai1889');
+
+if (!empty($_GET['search'])) {
+  $pdo = new PDO('mysql:host=localhost;dbname=blog', 'root', '+Koppai1889');
+  $query = $pdo->prepare
+  ('SELECT num_post, title, descrip, descrip_short, audio_link,
+  p.created_at, p.updated_at, p.num_category, name
+    FROM post
+    LEFT JOIN category ON post.num_category = category.num_category
+    WHERE title LIKE :search
+    ORDER BY created_at DESC
+    ');
+
+  $query->bindValue('search', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
+  $query->execute();
+
+  $posts = $query->fetchAll(PDO::FETCH_ASSOC);
+} else {
+  $query = $pdo->query
+  ('SELECT num_post, title, descrip, descrip_short, audio_link,
+  p.created_at, p.updated_at, p.num_category, name
+    FROM post p
+    LEFT JOIN category c ON p.num_category = c.num_category
+    ');
+  $posts = $query->fetchAll(PDO::FETCH_ASSOC);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
+  <title>Mario Music</title>
   <link rel="stylesheet" href="./styles.css" />
 </head>
 
 <body>
-  <a href="index.php" class="index">
-    <h1>Iconic <img src="./letter_m.png">ario Musics</h1>
-  </a>
-  <!-- N° 1 -->
-  <section class="podcast">
-    <h2 class="title">
-      <?php
-      include 'vars.php';
-      echo $podcast[0]['title'];
-      ?>
-    </h2>
-    <p class="descrip">
-      <?php
-      include 'vars.php';
-      echo $podcast[0]['descrip'];
-      ?>
-    </p>
-    <p class="listen">
-      <a href="podcast.php?id=0"><img src="./play-button.png" />Ecouter</a>
-    </p>
-  </section>
-  <!-- N° 2 -->
-  <section class="podcast">
-    <h2 class="title">
-      <?php
-      include 'vars.php';
-      echo $podcast[1]['title'];
-      ?>
-    </h2>
-    <p class="descrip">
-      <?php
-      include 'vars.php';
-      echo $podcast[1]['descrip'];
-      ?>
-    </p>
-    <p class="listen">
-      <a href="podcast.php?id=1"><img src="./play-button.png" />Ecouter</a>
-    </p>
-  </section>
-  <!-- N° 3 -->
-  <section class="podcast">
-    <h2 class="title">
-      <?php
-      include 'vars.php';
-      echo $podcast[2]['title'];
-      ?>
-    </h2>
-    <p class="descrip">
-      <?php
-      include 'vars.php';
-      echo $podcast[2]['descrip'];
-      ?>
-    </p>
-    <p class="listen">
-      <a href="podcast.php?id=2"><img src="./play-button.png" />Ecouter</a>
-    </p>
-  </section>
-  <!-- N° 4 -->
-  <section class="podcast">
-    <h2 class="title">
-      <?php
-      include 'vars.php';
-      echo $podcast[3]['title'];
-      ?>
-    </h2>
-    <p class="descrip">
-      <?php
-      include 'vars.php';
-      echo $podcast[3]['descrip'];
-      ?>
-    </p>
-    <p class="listen">
-      <a href="podcast.php?id=3"><img src="./play-button.png" />Ecouter</a>
-    </p>
-  </section>
-  <!-- N° 5 -->
-  <section class="podcast">
-    <h2 class="title">
-      <?php
-      include 'vars.php';
-      echo $podcast[4]['title'];
-      ?>
-    </h2>
-    <p class="descrip">
-      <?php
-      include 'vars.php';
-      echo $podcast[4]['descrip'];
-      ?>
-    </p>
-    <p class="listen">
-      <a href="podcast.php?id=4"><img src="./play-button.png" />Ecouter</a>
-    </p>
-  </section>
-  <!-- N° 6 -->
-  <section class="podcast">
-    <h2 class="title">
-      <?php
-      include 'vars.php';
-      echo $podcast[5]['title'];
-      ?>
-    </h2>
-    <p class="descrip">
-      <?php
-      include 'vars.php';
-      echo $podcast[5]['descrip'];
-      ?>
-    </p>
-    <p class="listen">
-      <a href="podcast.php?id=5"><img src="./play-button.png" />Ecouter</a>
-    </p>
-  </section>
-  <!-- N° 7 -->
-  <section class="podcast">
-    <h2 class="title">
-      <?php
-      include 'vars.php';
-      echo $podcast[6]['title'];
-      ?>
-    </h2>
-    <p class="descrip">
-      <?php
-      include 'vars.php';
-      echo $podcast[6]['descrip'];
-      ?>
-    </p>
-    <p class="listen">
-      <a href="podcast.php?id=6"><img src="./play-button.png" />Ecouter</a>
-    </p>
-  </section>
-  <!-- N° 8 -->
-  <section class="podcast">
-    <h2 class="title">
-      <?php
-      include 'vars.php';
-      echo $podcast[7]['title'];
-      ?>
-    </h2>
-    <p class="descrip">
-      <?php
-      include 'vars.php';
-      echo $podcast[7]['descrip'];
-      ?>
-    </p>
-    <p class="listen">
-      <a href="podcast.php?id=7"><img src="./play-button.png" />Ecouter</a>
-    </p>
-  </section>
-  <!-- N° 9 -->
-  <section class="podcast">
-    <h2 class="title">
-      <?php
-      include 'vars.php';
-      echo $podcast[8]['title'];
-      ?>
-    </h2>
-    <p class="descrip">
-      <?php
-      include 'vars.php';
-      echo $podcast[8]['descrip'];
-      ?>
-    </p>
-    <p class="listen">
-      <a href="podcast.php?id=8"><img src="./play-button.png" />Ecouter</a>
-    </p>
-  </section>
-  <!-- N° 10 -->
-  <section class="podcast">
-    <h2 class="title">
-      <?php
-      include 'vars.php';
-      echo $podcast[9]['title'];
-      ?>
-    </h2>
-    <p class="descrip">
-      <?php
-      include 'vars.php';
-      echo $podcast[9]['descrip'];
-      ?>
-    </p>
-    <p class="listen">
-      <a href="podcast.php?id=9"><img src="./play-button.png" />Ecouter</a>
-    </p>
-  </section>
-  <!-- N° 11 -->
-  <section class="podcast">
-    <h2 class="title">
-      <?php
-      include 'vars.php';
-      echo $podcast[10]['title'];
-      ?>
-    </h2>
-    <p class="descrip">
-      <?php
-      include 'vars.php';
-      echo $podcast[10]['descrip'];
-      ?>
-    </p>
-    <p class="listen">
-      <a href="podcast.php?id=10"><img src="./play-button.png" />Ecouter</a>
-    </p>
-  </section>
+  <div>
+    <a href="index.php" class="index">
+      <h1>Iconic <img src="./letter_m.png">ario Musics</h1>
+    </a>
+
+    <form method="GET">
+      <label for="search">Search</label>
+      <input type="search" name="search" id="search">
+    </form>
+
+    <?php if (empty($posts)): ?>
+      <p>No matching records found.</p>
+    <?php else: ?>
+      <?php foreach ($posts as $post): ?>
+        <section class="podcast">
+          <time class="created_at" datetime="<?= htmlspecialchars($post['created_at']) ?>">
+            <?= htmlspecialchars($post['created_at']) ?>
+          </time>
+          <h2 class="title">
+            <?= htmlspecialchars($post['title']) ?>
+          </h2>
+          <h3 class="category">
+            <?= htmlspecialchars($post['name']) ?>
+          </h3>
+          <pre class="descrip"> <?= htmlspecialchars($post['descrip_short']) ?> </pre>
+          <p class="listen">
+            <a href="podcast.php?num_post=<?= $post['num_post'] ?>">
+              <img src="./play-button.png" />Listen</a>
+          </p>
+        </section>
+      <?php endforeach ?>
+    <?php endif ?>
+
+    <div>
+      <?php if (!empty($status)): ?>
+        <p>
+          <?= $status ?>
+        </p>
+      <?php endif ?>
+      <form action="create.php" method="post" style="display: inline-flex; flex-direction: column">
+        <label for="title">Podcast Title</label>
+        <input type="text" name="title" id="title">
+        <select name="name" id="name">
+          <?php
+          $query_c = $pdo->query
+          ('SELECT DISTINCT p.num_category, name
+            FROM post p
+            LEFT JOIN category c ON p.num_category = c.num_category
+            ');
+          $category = $query_c->fetchAll(PDO::FETCH_ASSOC);
+          foreach ($category as $cat):
+            ?>
+            <option value="<?= htmlspecialchars($cat['num_category']) ?>">
+              <?= htmlspecialchars($cat['name']) ?>
+            </option>
+          <?php endforeach ?>
+        </select>
+        <label for="descrip_short">Short Description</label>
+        <textarea name="descrip_short" id="descrip_short" cols="30" rows="5"></textarea>
+        <label for="descrip">Description</label>
+        <textarea name="descrip" id="descrip" cols="30" rows="10"></textarea>
+        <label for="audio_link">Audio Link</label>
+        <textarea name="audio_link" id="audio_link" cols="30" rows="1"></textarea>
+        <input type="submit" value="Publish">
+      </form>
+    </div>
+
+    <div>
+      <form action="delete.php" method="post" style="display: inline-flex; flex-direction: column">
+        <label for="num_post">Delete Podcast</label>
+        <select name="num_post" id="num_post">
+          <?php foreach ($posts as $post): ?>
+            <option value="<?= htmlspecialchars($post['num_post']) ?>">
+              <?= htmlspecialchars($post['title']) ?>
+            </option>
+          <?php endforeach ?>
+        </select>
+        <input type="submit" value="Delete Podcast">
+      </form>
+    </div>
 </body>
 
 </html>
--- podcast.php
+++ podcast.php
@@ -1,23 +1,21 @@
 <?php
 
-include 'vars.php';
+$pdo = new PDO('mysql:host=localhost;dbname=blog', 'root', '+Koppai1889');
 
-$title;
-$controls;
-$error;
-$link;
-$home;
-
-
-if ($_GET["id"] > count($podcast)) {
-    $error = "Error 404<br>File Not found";
-    $link = "index.php";
-    $home = "Go to main page";
-} else {
-    $title = 'Iconic <img src="./letter_m.png">ario Musics';
-    $controls = "controls";
-}
-
+$query = $pdo->prepare('SELECT num_post, title, descrip, descrip_short, audio_link,
+    p.created_at, p.updated_at, p.num_category, name
+    FROM post p
+    LEFT JOIN category c ON p.num_category = c.num_category
+    WHERE num_post=:num_post
+  ');
+$query->bindValue('num_post', $_GET['num_post'], PDO::PARAM_STR);
+$query->execute();
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+/*
+if (empty($_GET['num_post']) ^ $_GET['num_post'] == 0) {
+    header('Location: 404.php');
+    die;
+}*/
 ?>
 
 <!DOCTYPE html>
@@ -25,61 +23,68 @@ if ($_GET["id"] > count($podcast)) {
 
 <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <meta name="viewport" content="wnum_postth=device-wnum_postth, initial-scale=1.0" />
+    <title>
+        <?php foreach ($posts as $post): ?>
+            <?= $post['title'] ?>
+        <?php endforeach ?>
+    </title>
     <link rel="stylesheet" href="./styles.css" />
 </head>
 
 <body>
-    <a href="index.php" class="index"><h1>
-        <?php
-        echo $title;
-        ?>
-    </h1></a>
-    <section class="podcast">
-        <h1 class="error">
-            <?php
-            echo $error;
-            ?>
-        </h1>
-        <h2 class="home">
-            <a href=<?php
-            echo $link;
-            ?>>
-        <?php
-        echo $home;
-        ?>
-            </a>
-        </h2>
-        <p class="date">
-            <?php
-            include 'vars.php';
-            echo $podcast[$_GET["id"]]['date'];
-            ?>
-        </p>
-        <h2 class="title">
-            <?php
-            include 'vars.php';
-            echo $podcast[$_GET["id"]]['title'];
-            ?>
-        </h2>
-        <p class="audio"><audio <?php
-        echo $controls;
-        ?>>
-                <source src="
-                <?php
-                include 'vars.php';
-                echo $podcast[$_GET["id"]]['audio'];
-                ?>
-            ">
-            </audio></p>
-        <p class="descrip">
-            <?php
-            include 'vars.php';
-            echo $podcast[$_GET["id"]]['descrip'];
-            ?>
-        </p>
-    </section>
+    <a href="index.php" class="index">
+        <h1>Iconic <img src="./letter_m.png">ario Musics</h1>
+    </a>
+
+    <?php if (empty($posts)): ?>
+        <p>No matching records found.</p>
+    <?php else: ?>
+        <?php foreach ($posts as $post): ?>
+            <section class="podcast">
+                <time class="created_at" datetime="<?= htmlspecialchars($post['created_at']) ?>">
+                    <?= htmlspecialchars($post['created_at']) ?>
+                </time>
+                <h2 class="title">
+                    <?= htmlspecialchars($post['title']) ?>
+                </h2>
+                <pre class="descrip"> <?= htmlspecialchars($post['descrip']) ?> </pre>
+                <p class="audio">
+                    <audio controls>
+                        <source src="<?= htmlspecialchars($post['audio_link']) ?>">
+                    </audio>
+                </p>
+            </section>
+        <?php endforeach ?>
+    <?php endif ?>
+
+    <div>
+        <?php if (!empty($status)): ?>
+            <p>
+                <?= $status ?>
+            </p>
+        <?php endif ?>
+        <form action="update.php?num_post=<?= $post['num_post'] ?>" method="post"
+            style="display: inline-flex; flex-direction: column">
+            <label for="title">Podcast Title</label>
+            <input type="text" name="title" id="title" value="<?= $post['title'] ?>">
+            <label for="descrip_short">Short Description</label>
+            <textarea name="descrip_short" id="descrip_short" cols="30"
+                rows="5"><?= $post['descrip_short'] ?></textarea>
+            <label for="descrip">Description</label>
+            <textarea name="descrip" id="descrip" cols="30" rows="10"><?= $post['descrip'] ?></textarea>
+            <select name="num_category" id="num_category">
+                <?php foreach ($posts as $post): ?>
+                    <option value="<?= htmlspecialchars($post['num_category']) ?>">
+                        <?= htmlspecialchars($post['name']) ?>
+                    </option>
+                <?php endforeach ?>
+            </select>
+            <label for="audio_link">Audio Link</label>
+            <textarea name="audio_link" id="audio_link" cols="30" rows="2"><?= $post['audio_link'] ?></textarea>
+            <input type="submit" value="Edit">
+        </form>
+    </div>
 </body>
 
 </html>
